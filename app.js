87fef4894b0ed4c774468f9895e0e7839c44a1bf
Jashem <mjashem@hotmail.com>
2019-01-11 22:42:46 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.render("search");
+});
+
+app.get("/results", function(req, res) {
+  var query = req.query.search;
+  var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      if (data.Response == "True") {
+        res.render("results", { data: data });
+      } else {
+        res.render("404");
+      }
+    } else {
+      res.render("404");
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Movie App has started!!");
+});
